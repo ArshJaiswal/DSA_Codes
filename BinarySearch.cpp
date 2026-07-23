8dcd47c0748ef5a6f47109ceef4f0cd245d7713f
ArshJaiswal <arshjaiswal121103@gmail.com>
2026-07-23 11:26:33 +0530

    Added new DSA solutions

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -164,3 +164,35 @@
 // }
 
 
+//Find a target in a rotated sorted array
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> nums={5,6,7,0,1,2,3,4};
+//     int target=10;
+//     int l=0,h=nums.size()-1;
+//     while(l<=h){
+//         int mid=l+(h-l)/2;
+//         if(nums[mid]==target){
+//             cout<<mid;
+//             return 0;
+//         }
+//         if(nums[l]<=nums[mid]){
+//             if(nums[l]<=target && nums[mid]>target){
+//                 h=mid-1;
+//             }
+//             else{
+//                 l=mid+1;
+//             }
+//         }else{
+//             if(nums[mid]<target && nums[h]>=target){
+//                 l=mid+1;
+//             }else{
+//                 h=mid-1;
+//             }
+//         }
+//     }
+//     cout<<-1;
+//     return 0;
+// }
